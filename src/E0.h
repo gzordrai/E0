a09d1c564c1d474b1d6e4b564c35c356db9f60bf
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-01-24 21:32:38 +0100

    adding parameters and constructors

--- src/E0.h
+++ src/E0.h
@@ -2,11 +2,19 @@
 #define E0_H
 
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class E0 {
+    private:
+        vector<int> key;
+        string message;
 
+    public:
+        E0(const string &key, const string &message);
+        E0(const vector<int> &key, const string &message);
 };
 
 #endif
