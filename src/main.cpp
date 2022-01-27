a62c500b17b41fd4cb7112bd8b8d10bf3f723d76
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-01-27 01:07:45 +0100

    added interaction

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,63 @@
 #include <iostream>
+#include <cstdlib>
 
 #include "E0.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    string key = "100111";
-    string msg = "11011101110000110111110110001011111001111100011111001101";
+    int choice;
+    bool stop = false;
+    string key, message;
 
-    E0 e = E0(key, msg);
-    cout << e.decrypt() << endl;
+    cout << "Please enter a key (6 bits):" << endl;
+    cin >> key;
+
+    while(key.size() != 6) {
+        cout << "Invalid key !" << endl;
+        cout << "Please enter a key (6 bits):" << endl;
+        cin >> key;
+    }
+
+    cout << "Please enter a message to encrypt/decrypt:" << endl;
+    cin >> message;
+
+    E0 e0 = E0(key, message);
+
+    while(!stop) {
+        do {
+            cout << "Please choose an action:\n1 - Change the key\n2 - Change the message to encrypt/decrypt\n3 - Encrypt the message\n4 - Decrypt the message\n5 - Stop" << endl;
+            cin >> choice;
+        } while(choice < 0 && choice > 5);
+
+        switch(choice) {
+            case 1:
+                do {
+                    cout << "Please enter a key (6 bits):" << endl;
+                    cin >> key;
+                } while(key.size() != 6);
+
+                e0.setKey(key);
+                break;
+
+            case 2:
+                cout << "Please enter a message to encrypt/decrypt:" << endl;
+                cin >> message;
+
+                e0.setMessage(message);
+                break;
+
+            case 3:
+                cout << e0.encrypt() << endl;
+                break;
+
+            case 4:
+                cout << e0.decrypt() << endl;
+                break;
+
+            case 5:
+                stop = true;
+                break;
+        }
+    }
 }
